f252b0b18a4a49e7216bda27e8759635b387cf77
xuyaokun_kzx <xuyaokun_kzx@alibaba.com>
2021-05-26 12:31:39 +0800

    impr:提交feign demo接口

---
+++ src/main/java/com/kunghsuspringcloud/eurekaclientone/controller/FeignDemoController.java
@@ -0,0 +1,20 @@
+package com.kunghsuspringcloud.eurekaclientone.controller;
+
+
+import com.kunghsuspringcloud.eurekaclientone.bean.ResultVo;
+import com.kunghsuspringcloud.eurekaclientone.service.HystrixDemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/feign")
+@RestController
+public class FeignDemoController {
+
+    @RequestMapping("/v1/result")
+    public ResultVo testHello(){
+        return ResultVo.valueOfSuccess("I am FeignDemoController from One");
+    }
+
+
+}
